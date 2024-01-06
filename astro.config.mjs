ef143ecd56241861f414e12d7dae83dfb5ed57b2
shawny-hpp <shawny@pissmail.com>
2024-01-07 00:57:51 +0100

    retry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shawny.github.io',
-  base: '/',
+  site: 'https://shawny.github.io'
 });
